9ff693500ae62daf010d02a47c1036566c6a21bc
natielybarbara <natiely.barbara@escola.pr.gov.br>
2024-11-13 08:41:48 -0300

    13-11

---
+++ sKetch.js
@@ -0,0 +1,10 @@
+condição: Escreva um programa que pergunte ao usuário sua idade
+
+let userAge = prompt ( "Quanto anos você tem?")
+userAge = Número ( uerAge );
+
+se ( usrAge >= 18 ) {
+  console . log ( " Você é maior de idade." );
+  } outro{
+  console . log ( "Você é de menor idade
+}
